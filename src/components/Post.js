50217a36120cb4d2a6b712738a8dda963abfecda
Yuliya <yulia.masechko@gmail.com>
2020-09-05 21:56:58 +0300

    new repository react app

---
+++ src/components/Post.js
@@ -0,0 +1,45 @@
+import React from "react";
+import 'normalize.css'
+import './post.css'
+import retweet from "./img/retweet.svg";
+import downArrow from "./img/down-arrow.svg";
+import comment from "./img/comment.svg";
+import share from "./img/share.svg";
+import warranty from "./img/warranty.svg";
+import heart from "./img/heart.svg";
+
+	function post(props) {
+		return (
+					<div className={'post'}>
+						<div className={"header"} >
+							<div className={'author'}>
+								<div className={'author-avatar'}>
+									<img src={props.author.photo} alt={props.author.name} />
+								</div>
+								<div className={'author-title'}>
+									<p>{props.author.name} <img className={"svg"} src={warranty} alt="" /> <span>{props.author.nickname}<span className={"dot"}>.</span>{props.date}</span></p>
+									<p>{props.content}</p>
+								</div>
+							</div>
+							<div>
+								<img src={downArrow} alt={"down arrow"}/>
+							</div>
+						</div>
+						<div className={"main"}>
+							<div className={'content'}>
+								<img src={props.image} alt={"Ray"}/>
+							</div>
+						</div>
+						<div className={"footer"}>
+							<div className={'icons'}>
+								<p><img src={comment} alt="" /><span>482</span></p>
+								<p><img src={retweet} alt=""/><span>146</span></p>
+								<p><img src={heart} alt="" /><span>887</span></p>
+								<p><img src={share} alt="" /></p>
+							</div>
+						</div>
+					 </div>
+		)
+	}
+
+export default post;
