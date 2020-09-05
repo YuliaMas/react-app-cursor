ddf45c9ae28bdd1d6666d8bf49badbc28d074a7d
Yuliya <yulia.masechko@gmail.com>
2020-09-06 00:03:39 +0300

    Updates

---
+++ precache-manifest.0741d98bdf34ddc5af0639259243cd24.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64701280d111c418c7d87a6f566240e4",
+    "url": "/react-app-cursor/index.html"
+  },
+  {
+    "revision": "6ff772d2f5d05320eedb",
+    "url": "/react-app-cursor/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "57c16f5b1b2a351d2f7c",
+    "url": "/react-app-cursor/static/css/main.4aed3e9f.chunk.css"
+  },
+  {
+    "revision": "6ff772d2f5d05320eedb",
+    "url": "/react-app-cursor/static/js/2.cd262901.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app-cursor/static/js/2.cd262901.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57c16f5b1b2a351d2f7c",
+    "url": "/react-app-cursor/static/js/main.acfb9863.chunk.js"
+  },
+  {
+    "revision": "58884499584256d169a0",
+    "url": "/react-app-cursor/static/js/runtime-main.1ffc3813.js"
+  },
+  {
+    "revision": "d87151d29ac177d15da795e3714161a6",
+    "url": "/react-app-cursor/static/media/comment.d87151d2.svg"
+  },
+  {
+    "revision": "d4521446c3a52ae3dabc8584f81299c2",
+    "url": "/react-app-cursor/static/media/down-arrow.d4521446.svg"
+  },
+  {
+    "revision": "b8519b2fbf49386742df7d1623d4e5e2",
+    "url": "/react-app-cursor/static/media/heart.b8519b2f.svg"
+  },
+  {
+    "revision": "e85ee3e0e5e550d695cac2c1de9055b1",
+    "url": "/react-app-cursor/static/media/retweet.e85ee3e0.svg"
+  },
+  {
+    "revision": "dfd3976ea15e4af844ba673fea834b32",
+    "url": "/react-app-cursor/static/media/share.dfd3976e.svg"
+  },
+  {
+    "revision": "baa3a5a6d57d5aec6a63a3f5d08631c3",
+    "url": "/react-app-cursor/static/media/warranty.baa3a5a6.svg"
+  }
+]);
